44f9a0d14fb7b0334d421c764d534ef3ad1047fe
Vadym Motsukh <vadym.motsukh@perfectial.com>
2014-06-16 18:28:13 +0300

    added not full tray ability

--- main.rb
+++ main.rb
@@ -1,5 +1,12 @@
-require 'screenshot'
+require 'tray_app';
+require 'screenshot';
+require 'test'
 
-app = Screenshot.new
+include Java
 
-app.start_shot
+app = TrayApplication.new("Robot")
+app.icon_filename = 'robot.png'
+app.item('Start tracker mag')  {t = Thread.new(Screenshot.new.start_shot); t.start;}
+app.item('Test Thread')  {t2 = Thread.new(); t2.start(Test.new.run_cycle);}
+app.item('Exit')              {java.lang.System::exit(0)}
+app.run
--- screenshot.rb
+++ screenshot.rb
@@ -5,16 +5,16 @@ class Screenshot
   import java.awt.Toolkit
   import java.awt.Rectangle
   import javax.imageio.ImageIO
-  import java.awt.Color
   import java.lang.Integer
   import java.io.FileInputStream
+  import java.lang.Runtime
 
     def get_image
       robot     = Robot.new
       toolkit   = Toolkit.get_default_toolkit
       dim       = toolkit.get_screen_size
       m_width = dim.get_width
-      rectangle = Rectangle.new(1300, 0, m_width-m_width*0.85, 24) #TODO: need to change val 200
+      rectangle = Rectangle.new(m_width*0.5, 0, m_width-m_width*0.75, 24) #TODO: need to change val 200
       image     = robot.create_screen_capture(rectangle)
     end
 
@@ -25,37 +25,23 @@ class Screenshot
       @i = 0
       while @i < iw  do
         temp = img.getRGB(@i,12)
-        # hex = Integer.toHexString(temp)[2..-1]
         hex = Integer.toHexString(temp)
-        # c = Color.new(temp)
-        # p c.getRed
-        # p c.getGreen
-        # p '/////////////////////'
-        # p hex
-        # TODO: add color for disable state
         break if (hex == 'ff373737' || hex == 'ffac0d0d' || hex == 'ffff9e0d' || hex == 'ff598b36' || hex == 'ff141414')
         @i +=1
       end
       @i+1 < iw ? @i+1 : nil
     end
 
-    def record_in_file
-      file = java::io::File.new('test.png')
-      ImageIO::write(@image, "png", file)
-    end
-
     def start_shot
       p '-------------start'
-      x_t ||= nil
-      image = get_image
-      # p "".blank?
-      unless x_t.nil?
+      @x_t ||= nil
+      unless @x_t.nil?
         p 'work_cycle'
-        p x_t
-        work_cycle x_t, image
+        p @x_t
+        work_cycle @x_t
       else
         p 'start_shot else'
-        x_t = find_position_cycle image
+        @x_t = find_position_cycle
         self.start_shot
       end
     end
@@ -63,13 +49,15 @@ class Screenshot
     def work_cycle x_t
       loop do
         timer_color = nil
+        image = get_image
         timer_color = image.getRGB(x_t, 12) unless x_t.nil?
-        # take_image
+        w_title = %w[chrome sublime].sample
         case Integer.toHexString(timer_color)
         when 'ffac0d0d'
           p '===================3'
         when 'ffff9e0d'
           p '===================2'
+          self.execute('wmctrl -R'+ w_title)
         when 'ff598b36'
           p '===================1'
         when 'ff141414'
@@ -79,22 +67,20 @@ class Screenshot
       end
     end
 
-    def find_position_cycle image
+    def find_position_cycle
       x_t = nil
       loop do
-        p 'find_position_cycle'
-        p image
+        image = get_image
         x_t = find_timer_position(image)
-        p x_t
         break unless x_t.nil?
         sleep(1)
       end
       x_t
     end
 
-    def take_image
-      file = java::io::File.new("test.png_#{Time.now.to_i}")
-      ImageIO::write(@image, "png", file)
+    def take_image image
+      file = java::io::File.new("test_#{Time.now.to_i}.png")
+      ImageIO::write(image, "png", file)
     end
 
 
@@ -106,5 +92,9 @@ class Screenshot
       # ffff9e0d orange 2
       # ffac0d0d red 3
       # ff141414 screen
+    end
+
+    def execute cmd
+      Runtime.getRuntime.exec("#{cmd}")
     end
 end
